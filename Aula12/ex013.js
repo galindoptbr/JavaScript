dbe7817ca69b110de62f367485be5c6a2f7dea6a
Adsandro Galindo Leite Filho <112688705+galindoptbr@users.noreply.github.com>
2022-11-30 19:08:46 +0000

    Update ex013.js

--- Aula12/ex013.js
+++ Aula12/ex013.js
@@ -3,25 +3,25 @@ var diaSem = agora.getDay()
 
 switch(diaSem) {
     case 0:
-        console.log('Domingo')
+        console.log('Hoje é Domingo!')
         break
     case 1:
-        console.log('Segunda')
+        console.log('Hoje é Segunda!')
         break
     case 2:
-        console.log('Terça')
+        console.log('Hoje é Terça!')
         break
     case 3:
-        console.log('Quarta')
+        console.log('Hoje é Quarta!')
         break
     case 4:
-        console.log('Quinta')
+        console.log('Hoje é Quinta!')
         break
     case 5:
-        console.log('Sexta')
+        console.log('Hoje é Sexta!')
         break
     case 6:
-        console.logo('Sábado')
+        console.logo('Hoje é Sábado!')
         break
     default:
         console.log('[ERRO] Dia inválido.')
