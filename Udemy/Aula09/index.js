322c17fa03f305e5526270dae8081efdd694aa6d
Adsandro Galindo Leite Filho <112688705+galindoptbr@users.noreply.github.com>
2022-12-22 22:52:13 +0000

    estudos 22 12

---
+++ Udemy/Aula09/index.js
@@ -0,0 +1,3 @@
+const n1 = 5;
+const n2 = 10;
+console.log(n1 / n2);
---
+++ Udemy/Aula10/script.js
@@ -0,0 +1,6 @@
+let n1 = prompt('Digite um número: ');
+let n2 = prompt('Digite outro número. ');
+n1 = Number(n1);
+n2 = Number(n2);
+const resultado = n1 + n2;
+window.alert(`O resultado é ${resultado}.`)
