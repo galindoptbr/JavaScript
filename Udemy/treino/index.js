3a0ad18fff0a50efdce058186fbc9d7fe002aa8b
Adsandro Galindo Leite Filho <112688705+galindoptbr@users.noreply.github.com>
2023-01-01 18:17:27 +0000

    pasta de treino

---
+++ Udemy/treino/index.js
@@ -0,0 +1,14 @@
+let tenhoGrana = true;
+let estaSol = false;
+let chaveCarro = false;
+
+const sairdecasa = 'Vou sair de Casa!';
+const ficaremcasa = 'Vou ficar em Casa!'
+
+if (tenhoGrana && estaSol && chaveCarro) {
+    console.log(sairdecasa);
+} else if (tenhoGrana && estaSol) {
+    console.log(sairdecasa);
+} else {
+    console.log(ficaremcasa);
+}
