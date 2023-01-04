f1bbbabe528588d5f81a3a7c01873c6e31d973f2
Adsandro Galindo Leite Filho <112688705+galindoptbr@users.noreply.github.com>
2023-01-04 00:00:40 +0000

    estudos 03 01 2023

---
+++ Udemy/aula26/index.js
@@ -0,0 +1,22 @@
+// ? :
+
+//OPERACAO TERNARIA
+
+const pontUsuario = 400;
+// (condicao) ? 'valor para verdadeiro' : 'vamor para falso';
+const nivelUsuario = pontUsuario >= 1000 ? 'Usuario VIP' : 'Usuario NORMAL';
+console.log(nivelUsuario);
+
+
+const corUsuario = null;
+const corPadrao = corUsuario || 'Black';
+console.log(corPadrao);
+
+/* if (pontUsuario >= 1000) {
+    console.log('Usuario VIP.');    
+} else {
+    console.log('Usuario NORMAL.')
+}
+*/
+
+console.log(25 / 1.5);
