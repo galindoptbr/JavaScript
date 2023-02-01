ea1b1eeff88c12bb96ec1636591a262b122c30d7
Adsandro Galindo Leite Filho <112688705+galindoptbr@users.noreply.github.com>
2023-02-01 21:10:17 +0000

    estudos janeiro

--- Udemy/lista-de-tarefas/js/main.js
+++ Udemy/lista-de-tarefas/js/main.js
@@ -1,4 +1,60 @@
 let tarefa = document.querySelector('.tarefa');
 let adicionar = document.querySelector('.adicionar');
+let tarefas = document.querySelector('.tarefas');
 
-  
+function criaLi() {
+    const li = document.createElement('li');
+    return li;
+}
+
+tarefa.addEventListener('keypress', function(e) {
+    if (e.keyCode === 13) {
+        if (!tarefa.value) return;
+        criaTarefa(tarefa.value);
+    }
+});
+
+function limpaInput() {
+    tarefa.value = '';
+    tarefa.focus();
+}
+
+function criaBotaoApagar(li) {
+    li.innerText += ' ';
+    const botaoApagar = document.createElement('button');
+    botaoApagar.innerText = 'Apagar';
+    botaoApagar.setAttribute('class', 'apagar' );
+    botaoApagar.setAttribute('title', 'apagar esta tarefa' );
+    li.appendChild(botaoApagar);
+}
+
+function criaTarefa(textoInput) {
+    const li = criaLi();
+    li.innerText = textoInput;
+    tarefas.appendChild(li);
+    limpaInput();
+    criaBotaoApagar(li);
+}
+
+adicionar.addEventListener('click', function() {
+    if (!tarefa.value) return;
+    criaTarefa(tarefa.value);
+});
+
+document.addEventListener('click', function(e) {
+    const el = e.target;
+
+    if (el.classList.contains('apagar')) {
+        el.parentElement.remove();
+    }
+});
+
+function salvarTarefas() {
+    const liTarefas = tarefas.querySelectorAll('li');
+    const listaDetarefas = [];
+
+    for (let afazer of liTarefas) {
+        let tarefa = tarefas.innerText;
+        tarefa = tarefa.replace('apagar', '');
+    }
+}
