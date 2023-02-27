6bc65a2756a39a30aba1b8abfcace29f4eea389d
Adsandro Galindo Leite Filho <112688705+galindoptbr@users.noreply.github.com>
2023-02-27 23:00:47 +0000

    27 02

--- Udemy/aula56/script.js
+++ Udemy/aula56/script.js
@@ -1,1 +1,10 @@
- 
+ function Calculadora() {
+    this.display = document.querySelector('.display');
+
+    this.inicia = () => {
+        
+    }
+ }
+
+ const calculadora = new Calculadora();
+ calculadora.inicia();
--- Udemy/treino/script.js
+++ Udemy/treino/script.js
@@ -1,9 +1,38 @@
-const toggleButton = document.querySelector('.botao');
+function soma(a, b) {
+    let resultado = a + b;
+    console.log(resultado);
+}
 
-toggleButton.addEventListener('click', function() {
-    if (document.body.style.backgroundColor === 'white') {
-        document.body.style.backgroundColor = 'orange';
+soma(9, 3);
+
+function quadrado(num) {
+    let quad = num * num;
+    console.log(quad);
+}
+
+quadrado(3);
+
+function fatorial(num) {
+    if (num === 0 || num === 1) {
+        return 1;
+        
     } else {
-        document.body.style.backgroundColor = 'white';
+        return num * fatorial(num - 1);
+        
     }
-});
+}
+
+console.log(fatorial(5));
+
+function par(num) {
+    if (num % 2 === 0) {
+        return 'PAR';
+    } else {
+        return 'IMPAR';
+    }
+}
+
+console.log(par(26));
+
+
+
