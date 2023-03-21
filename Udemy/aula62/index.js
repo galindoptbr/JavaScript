16c2a151c57106680568225ccd6fe3d3d05e6246
Adsandro Galindo Leite Filho <112688705+galindoptbr@users.noreply.github.com>
2023-03-21 22:56:22 +0000

    filter map reduce

--- Udemy/aula62/index.js
+++ Udemy/aula62/index.js
@@ -1,22 +1,22 @@
-const pessoas = [
-    {nome: 'Pedro', idade: 64},
-    {nome: 'Maria', idade: 72},
-    {nome: 'Jose', idade: 20},
-    {nome: 'Ramom', idade: 7},
-    {nome: 'Luzia', idade: 51},
-];
+// const pessoas = [
+//     {nome: 'Pedro', idade: 64},
+//     {nome: 'Maria', idade: 72},
+//     {nome: 'Jose', idade: 20},
+//     {nome: 'Ramom', idade: 7},
+//     {nome: 'Luzia', idade: 51},
+// ];
 
-const comId = pessoas.map(function(obj, indice) {
-    obj.id = (indice +1);
-    return obj;
-});
+// const comId = pessoas.map(function(obj, indice) {
+//     obj.id = (indice +1);
+//     return obj;
+// });
 
-console.log(comId);
+// console.log(comId);
 
 const num = [10, 4, 44, 3, 2, 1, 55, 63, 32, 27];
+const numPar = num
+    .filter(valor => valor % 2 === 0)
+    .map(valor => valor * 2)
+    .reduce((ac, valor) => ac + valor);
 
-const maisVelha = pessoas.reduce(function(acumulador, valor) {
-    if(acumulador.idade > valor.idade) return acumulador;
-    return valor;
-});
-console.log(maisVelha);
+console.log(numPar);
